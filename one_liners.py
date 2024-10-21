f625754b0b70566893603f3d265f6d26dbaf833c
Connor Kim <152044036+ShrugShoulders@users.noreply.github.com>
2024-10-20 18:08:13 -0600

    Update one_liners.py

--- one_liners.py
+++ one_liners.py
@@ -1,3 +1,3 @@
 print(''.join(f"{'\x0304' if i % 2 == 0 else '\x0307'}{c}" for i, c in enumerate("hadokn")))
 print(''.join(f'\x030{5*(i%3)**2-8*(i%3)+3}{c}' for i,c in enumerate('hadokn')))
-import random; print(f"\x03{random.randint(0, 15)}" + random.choice([r'¯\\_(ツ)_/¯', 'ヽ(´ー｀)┌', r'¯\\(°_o)/¯', '乁( •_• )ㄏ', '┐(´д｀)┌']) + "\x03")
+import random; print(f"\x03{random.randint(0, 15)}" + random.choice([r'¯\_(ツ)_/¯', 'ヽ(´ー｀)┌', r'¯\(°_o)/¯', '乁( •_• )ㄏ', '┐(´д｀)┌', 'it really tied the room together']) + "\x03")
